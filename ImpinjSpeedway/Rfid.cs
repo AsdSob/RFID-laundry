6635c1c02b850f52dfa8946e31f51b8b3244b054
SobirASD <34741911+SobirASD@users.noreply.github.com>
2019-11-17 15:48:38 +0400

    rfid test

--- ImpinjSpeedway/Rfid.cs
+++ ImpinjSpeedway/Rfid.cs
@@ -1,23 +1,78 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Impinj.OctaneSdk;
 
 namespace ImpinjSpeedway
 {
     public class Rfid
     {
-        public static ImpinjReader Reader = new ImpinjReader();
-        public static Settings ReaderSetting = new Settings();
+        public static bool IsConnected;
 
         static void Main(string[] args)
         {
+            RfidCommands reader = new RfidCommands();
+
             Console.WriteLine("Begin--------");
 
-            Reader.Connect();
-            Reader.Address();
+            bool exit = false;
+            ConsoleKey enterKey;
+
+            while (!exit)
+            {
+                enterKey = Console.ReadKey().Key;
+
+                switch (enterKey)
+                {
+                    case ConsoleKey.Tab:
+                        IsConnected = reader.Connection();
+                        Console.WriteLine($"Reader Connected - {IsConnected}");
+                        break;
+
+                    case ConsoleKey.Q:
+                        reader.Antenna();
+                        break;
+
+                    case ConsoleKey.W:
+                        reader.Start();
+                        break;
+
+                    case ConsoleKey.E:
+                        reader.Stop();
+                        break;
+
+                    case ConsoleKey.A:
+                        exit= true;
+                        break;
+
+                    case ConsoleKey.Z:
+                        reader.Reader.TagsReported += DisplayTag;
+                        break;
+                    case ConsoleKey.X:
+                        reader.Reader.TagsReported -= DisplayTag;
+                        break;
+
+
+                }
+
+            }
+
+            reader.Reader.TagsReported += DisplayTag;
+
+            Console.WriteLine("\n Disconnected !!!!!");
+            Console.ReadKey();
+
         }
+
+        private static void DisplayTag(ImpinjReader reader, TagReport report)
+        {
+            foreach (Tag tag in report)
+            {
+                if (tag.Epc != null)
+                {
+                    Console.WriteLine(
+                        $"Reader= {reader.Name} /Antenna= {tag.AntennaPortNumber} /EPC= {tag.Epc} /RSS= {tag.PeakRssiInDbm} /Frequency= {tag.ChannelInMhz} /Phase= {tag.PhaseAngleInRadians}");
+                }
+            }
+        }
+
     }
 }
