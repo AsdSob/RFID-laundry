60dfe29af0d37b7afb92101a01e4b67a8144f973
SobirASD <34741911+SobirASD@users.noreply.github.com>
2019-11-26 00:10:31 +0400

    moves

--- TestConveyor/Svoyo/Data/Client.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestConveyor.Svoyo.Data
-{
-    public class Client : NameEntity
-    {
-        public int ParentId { get; set; }
-        public string ShortName { get; set; }
-        public bool Active { get; set; }
-    }
-}
--- TestConveyor/Svoyo/Data/ClientInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestConveyor.Svoyo.Data
-{
-    public class ClientInfo : Entity
-    {
-        public string Address { get; set; }
-        public int CityId { get; set; }
-        public int ClientId { get; set; }
-    }
-}
