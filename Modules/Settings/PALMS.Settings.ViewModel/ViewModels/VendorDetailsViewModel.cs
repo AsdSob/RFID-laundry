e240c478637045b04516b1f308ed5881e4367e17
SobirASD <34741911+SobirASD@users.noreply.github.com>
2019-12-07 05:48:38 +0400

    test

--- Modules/Settings/PALMS.Settings.ViewModel/ViewModels/VendorDetailsViewModel.cs
+++ Modules/Settings/PALMS.Settings.ViewModel/ViewModels/VendorDetailsViewModel.cs
@@ -15,7 +15,7 @@ using PALMS.Settings.ViewModel.EntityViewModels;
 using PALMS.Settings.ViewModel.Windows;
 using PALMS.ViewModels.Common;
 using PALMS.ViewModels.Common.Services;
-using PlcControl;
+
 
 namespace PALMS.Settings.ViewModel.ViewModels
 {
@@ -789,34 +789,34 @@ namespace PALMS.Settings.ViewModel.ViewModels
 
         private void HangToBeltSlot(FinsTcp belt, int slotNumb)
         {
-            //// Подготовка слота 
-            //if (belt.GetNowPoint() != slotNumb)
-            //{
-            //    belt.GetModel();
-            //    belt.SetNowPoint(slotNumb);
-            //    belt.GotoPoint();
+            // Подготовка слота 
+            if (belt.GetNowPoint() != slotNumb)
+            {
+                belt.GetModel();
+                belt.SetNowPoint(slotNumb);
+                belt.GotoPoint();
 
-            //    //belt.SetNowPoint(slotNumb);
-            //    //belt.GetNowPoint();
-            //    //belt.GotoPoint();
+                //belt.SetNowPoint(slotNumb);
+                //belt.GetNowPoint();
+                //belt.GotoPoint();
 
-            //    // ожыдание окончание подготовки слота в линии
-            //    while (belt.DialState())
-            //    {
-            //        Thread.Sleep(500);
-            //    }
-            //}
+                // ожыдание окончание подготовки слота в линии
+                while (belt.DialState())
+                {
+                    Thread.Sleep(500);
+                }
+            }
 
-            //// начала загрузки в слот
-            //var isHangWorking = false;
-            //while (!isHangWorking)
-            //{
-            //    belt.Hang_In();
-            //    Thread.Sleep(500);
-            //    isHangWorking = belt.GetClotheinhook();
-            //}
+            // начала загрузки в слот
+            var isHangWorking = false;
+            while (!isHangWorking)
+            {
+                belt.Hang_In();
+                Thread.Sleep(500);
+                isHangWorking = belt.GetClotheinhook();
+            }
 
-            belt.HangUpToPoint(slotNumb);
+            //belt.HangUpToPoint(slotNumb);
 
             var getClothInHook = false;
             while (!getClothInHook)
