95d7b8c2f1a6cf224ac270b77d926814a13ed690
SobirASD <34741911+SobirASD@users.noreply.github.com>
2019-12-08 12:58:48 +0400

    test core 3

---
+++ TestApps/ImpinjTest/ImpinjTest/Rfid.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Impinj.OctaneSdk;
+
+namespace ImpinjTest
+{
+    public class Rfid
+    {
+        public static RfidCommon Impinj = new RfidCommon();
+        public static int Setchik;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var isRunning = true;
+
+            while (isRunning)
+            {
+                var key = Console.ReadKey();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.Q:
+                        isRunning = false;
+                        break;
+
+                    case ConsoleKey.Tab:
+                        Start();
+                        break;
+
+                    case ConsoleKey.Z:
+                        Console.WriteLine(" +10.0 to +31.5 | Enter TxPower=>");
+                        double txPower = Convert.ToDouble(Console.ReadLine());
+                        TXSet(txPower);
+                        break;
+
+                    case ConsoleKey.X:
+                        Console.WriteLine(" -85 to 0 | Enter RxPower=>");
+                        double rxPower = Convert.ToDouble(Console.ReadLine());
+                        RXSet(rxPower);
+                        break;
+
+                    case ConsoleKey.A:
+                        Task.Factory.StartNew(StartShowTags);
+                        break;
+
+                    case ConsoleKey.S:
+                        StopShowTags();
+                        break;
+                }
+            }
+        }
+
+        public static void Start()
+        {
+            Impinj.Connection();
+            Console.WriteLine("Connected");
+        }
+
+        public static void StartShowTags()
+        {
+            Setchik = 0;
+            Impinj.Reader.Start();
+            Impinj.Reader.TagsReported += ShowTags;
+            Console.WriteLine("---Started---");
+        }
+
+        public static void StopShowTags()
+        {
+            Impinj.Reader.TagsReported -= ShowTags;
+            Impinj.Reader.Stop();
+            Console.WriteLine("---Stopped---");
+        }
+
+        public static void ShowTags(ImpinjReader reader, TagReport report)
+        {
+
+            foreach (var tag in report.Tags.OrderBy(x => x.AntennaPortNumber))
+            {
+                Setchik++;
+                Console.WriteLine($"EPC== {tag.Epc} |||||== {Setchik}");
+            }
+        }
+
+        public static void TXSet(double power)
+        {
+            for (ushort i = 1; i <= 4; i++)
+            {
+                Impinj.settings.Antennas.GetAntenna(i).TxPowerInDbm = Convert.ToDouble(power);
+            }
+
+            Impinj.Reader.ApplySettings(Impinj.settings);
+        }
+
+        public static void RXSet(double power)
+        {
+            for (ushort i = 1; i <= 4; i++)
+            {
+                Impinj.settings.Antennas.GetAntenna(i).RxSensitivityInDbm = power;
+            }
+
+            Impinj.Reader.ApplySettings(Impinj.settings);
+        }
+
+    }
+}
---
+++ TestApps/ImpinjTest/ImpinjTest/RfidCommon.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Impinj.OctaneSdk;
+
+namespace ImpinjTest
+{
+    public class RfidCommon
+    {
+        public ImpinjReader Reader = new ImpinjReader();
+        public Impinj.OctaneSdk.Settings settings;
+
+        private ConcurrentDictionary<int, ConcurrentDictionary<string, Tuple<DateTime?, DateTime?>>> _data =
+            new ConcurrentDictionary<int, ConcurrentDictionary<string, Tuple<DateTime?, DateTime?>>>();
+
+        public bool Connection()
+        {
+            try
+            {
+                if (Reader.IsConnected)
+                {
+                    Reader.Disconnect();
+                }
+
+                Reader.Connect("192.168.250.55");
+                Reader.Stop();
+
+            }
+            catch (OctaneSdkException ee)
+            {
+                Console.WriteLine("Octane SDK exception: Reader #1" + ee.Message, "error");
+            }
+            catch (Exception ee)
+            {
+                // Handle other .NET errors.
+                Console.WriteLine("Exception : Reader #1" + ee.Message, "error");
+                Console.WriteLine(ee.StackTrace);
+            }
+
+            SetSettings();
+            return Reader.IsConnected;
+        }
+
+        private void SetSettings()
+        {
+            settings = Reader.QueryDefaultSettings();
+
+            settings.Report.IncludeAntennaPortNumber = true;
+            settings.Report.IncludePhaseAngle = true;
+            settings.Report.IncludeChannel = true;
+            settings.Report.IncludeDopplerFrequency = true;
+            settings.Report.IncludeFastId = true;
+            settings.Report.IncludeFirstSeenTime = true;
+            settings.Report.IncludeLastSeenTime = true;
+            settings.Report.IncludePeakRssi = true;
+            settings.Report.IncludeSeenCount = true;
+            settings.Report.IncludePcBits = true;
+            settings.Report.IncludeSeenCount = true;
+
+            settings.ReaderMode = ReaderMode.AutoSetDenseReaderDeepScan;//.AutoSetDenseReader;
+            settings.SearchMode = SearchMode.DualTarget;//.DualTarget;
+            settings.Session = 1;
+            settings.TagPopulationEstimate = Convert.ToUInt16(200);
+
+            settings.Report.Mode = ReportMode.Individual;
+
+            Antenna();
+
+            Reader.ApplySettings(settings);
+        }
+
+        public void Antenna()
+        {
+            settings.Antennas.DisableAll();
+            var j = settings.Antennas.AntennaConfigs.Count;
+
+            for (ushort i = 1; i <= j; i++)
+            {
+                settings.Antennas.GetAntenna(i).IsEnabled = true;
+                settings.Antennas.GetAntenna(i).TxPowerInDbm = Convert.ToDouble("15");
+                settings.Antennas.GetAntenna(i).RxSensitivityInDbm = Convert.ToDouble("-70");
+            }
+        }
+
+        public void Start()
+        {
+            Reader.Start();
+        }
+
+        public void Stop()
+        {
+            Reader.Stop();
+        }
+
+
+        public ConcurrentDictionary<int, ConcurrentDictionary<string, Tuple<DateTime?, DateTime?>>> GetSortedTags(int readTime)
+        {
+            StartRead();
+
+            Thread.Sleep(readTime);
+
+            var data = StopRead();
+            return data;
+        }
+
+        public void StartRead()
+        {
+            Connection();
+
+            Start();
+            Reader.TagsReported += DisplayTag;
+        }
+
+        public ConcurrentDictionary<int, ConcurrentDictionary<string, Tuple<DateTime?, DateTime?>>> StopRead()
+        {
+            Reader.TagsReported -= DisplayTag;
+            Stop();
+            Reader.Disconnect();
+            return _data;
+        }
+
+        private void DisplayTag(ImpinjReader reader, TagReport report)
+        {
+            foreach (Tag tag in report)
+            {
+                AddData(tag.AntennaPortNumber, tag.Epc.ToString(), tag.LastSeenTime.LocalDateTime);
+            }
+        }
+
+        private void AddData(int antenna, string epc, DateTime time)
+        {
+            // проверка ест ли словарь антенны 
+            if (!_data.TryGetValue(antenna, out ConcurrentDictionary<string, Tuple<DateTime?, DateTime?>> val))
+            {
+                val = new ConcurrentDictionary<string, Tuple<DateTime?, DateTime?>>();
+
+                _data.TryAdd(antenna, val);
+            }
+
+            // проверка на наличие чипа в словаре
+            if (!val.TryGetValue(epc, out Tuple<DateTime?, DateTime?> times))
+            {
+                times = new Tuple<DateTime?, DateTime?>(time, null);
+                val.TryAdd(epc, times);
+            }
+            else
+            {
+                val.TryUpdate(epc, new Tuple<DateTime?, DateTime?>(times.Item1, time), times);
+                //данные можно сохранять в БД, но метке можно обнулить
+            }
+
+            // если метка повторно падает в считыватель, то нужно предыдущие данные сохранять в БД
+        }
+
+    }
+}
